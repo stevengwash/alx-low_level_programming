73fe9d11a41eeb52d11984307c8ecd41136e7f40
ngwash <stevengwash@gmail.com>
2024-05-02 17:04:49 +0300

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * -isdigit checking for digits (0 to 9)
+ *  @c inter to be checked
+ *  return: 1 if c is a digit, 0 if not a digit
+ */
+int _isdigit(int c);
+{
+return (c >='0' && c <=9);
+}
+
