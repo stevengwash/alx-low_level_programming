e3c8c6f7179845e17cc4414c828f691e425ec8ea
ngwash <stevengwash@gmail.com>
2024-05-02 16:08:34 +0300

    0. isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * -isupper checking for uppercase programme
+ * @c: variable text 
+ * return  always 0.
+ */
+int _isupper(int c);
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else 
+{
+return (0);
+}
+}
